9763a4352e748da4e6f3aa629ec075ebf76d2ffa
choi <ramongrupy@gmail.com>
2025-10-18 17:24:05 +0900

    user 내용 추가

--- MyClass/User.java
+++ MyClass/User.java
@@ -1,23 +1,25 @@
 package MyClass;
 
-
 /**
  * User 클래스의 설명을 작성하세요.
  *
  * @author (작성자 이름)
  * @version (버전 번호 또는 작성한 날짜)
  */
-public class User extends DB_Element
-{
-    
+public class User extends DB_Element {
+    private String studentID;
     private String name;
-    private Integer stID;
-    
-    //2개의 파라메터의 반환값을 넣을 수 있도록 함
-    public User(){
-        
+
+    public User(String studentID, String name) {
+        this.studentID = studentID;
+        this.name = name;
     }
-    public String getID(){
-        return "";
+
+    public String getID() {
+        return studentID;
+    }
+
+    public String toString() {
+        return studentID + " / " + name;
     }
 }
