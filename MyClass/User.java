f6fd61603b4882682c934e4d28d9e2af83691156
PumpkinRamen <90686733+PumpkinRamen@users.noreply.github.com>
2025-10-20 18:28:12 +0900

    Update User.java

--- MyClass/User.java
+++ MyClass/User.java
@@ -1,25 +1,41 @@
 package MyClass;
 
 /**
- * User 클래스의 설명을 작성하세요.
+ * 도서관 이용자 정보를 나타내는 User클래스
  *
- * @author (작성자 이름)
- * @version (버전 번호 또는 작성한 날짜)
+ * @author (2024320007 정회서)
+ * @version (2025.10.20)
  */
 public class User extends DB_Element {
     private Integer studentID;
     private String name;
 
+    /**
+     *  User 클래스의 객체 생성자
+     *
+     *  @param  studentID 이용자의 학번
+     *  @param  name      이용자의 이름
+     */
     public User(Integer studentID, String name) {
         this.studentID = studentID;
         this.name = name;
     }
 
+    /**
+     * 이용자의 학번을 toString 형태의 문자열로 반환하는 메소드
+     *
+     * @return    이용자의 학번 반환
+     */
     public String getID() {
         return studentID.toString();
     }
 
+    /**
+     * 이용자의 학번과 이름을 toString 형태의 문자열로 반환하는 메소드
+     *
+     * @return    "학번 / 이름" 형태로 반환
+     */
     public String toString() {
-        return "[" + studentID +"]" + " / " + name;
+        return studentID + " / " + name;
     }
 }
