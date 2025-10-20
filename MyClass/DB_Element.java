98543a22b6a4838617bb62ef42a06b785868e8fb
PumpkinRamen <90686733+PumpkinRamen@users.noreply.github.com>
2025-10-20 18:28:00 +0900

    Revise documentation for DB_Element class
    Updated class documentation for DB_Element.

--- MyClass/DB_Element.java
+++ MyClass/DB_Element.java
@@ -1,13 +1,15 @@
 package MyClass;
 
-
 /**
- * DB_Element 클래스의 설명을 작성하세요.
+ * 도서관 DB의 공통 인터페이스인 추상 클래스
  *
- * @author (작성자 이름)
- * @version (버전 번호 또는 작성한 날짜)
+ * @author (2024320007 정회서)
+ * @version (2025.10.20)
  */
 public abstract class DB_Element
 {
+     /**
+     * 요소의 고유 ID를 반환하는 메소드
+     */
     public abstract String getID();
 }
